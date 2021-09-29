5942904aa6d78b84390d2841194dd0513d6beade
xiayuhu <jshzxiayuhu@126.com>
2021-09-29 21:14:12 +0800

    spring继续整合mybatis

---
+++ src/main/java/com/xiayuhu/ssm/Library_Management_System/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.xiayuhu.ssm.Library_Management_System.service.impl;
+
+import com.xiayuhu.ssm.Library_Management_System.entity.User;
+import com.xiayuhu.ssm.Library_Management_System.mapper.UserMapper;
+import com.xiayuhu.ssm.Library_Management_System.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public List<User> listUser() {
+        List<User> userList = userMapper.selectUser();
+        for (int i = 0; i < userList.size(); i++) {
+            System.out.println(userList);
+        }
+        return userList;
+    }
+}
--- src/test/java/com/xiayuhu/ssm/Library_Management_System/aaTest.java
+++ src/test/java/com/xiayuhu/ssm/Library_Management_System/aaTest.java
@@ -2,16 +2,23 @@ package com.xiayuhu.ssm.Library_Management_System;
 
 import com.xiayuhu.ssm.Library_Management_System.entity.User;
 import com.xiayuhu.ssm.Library_Management_System.mapper.UserMapper;
+
+import com.xiayuhu.ssm.Library_Management_System.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 public class aaTest {
+
+
     @Test
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-mybatis.xml");
-        UserMapper userMapper = (UserMapper) context.getBean("userMapper");
-        User user = (User) userMapper.selectUser();
+    public void select( ) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-mybatis.xml");
+        UserMapper userMapper =  context.getBean(UserMapper.class);
+
+        List<User> user = userMapper.selectUser();
         System.out.println(user);
 
     }
